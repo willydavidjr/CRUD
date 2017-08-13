b7fa6db76e993de23afec174c329e6b54021b5f8
hope.baclagon <hope.baclagon@DESKTOP-IEGTH1U>
2017-08-13 18:26:47 +0800

    Add project files.

---
+++ CRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUD.Startup))]
+namespace CRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
